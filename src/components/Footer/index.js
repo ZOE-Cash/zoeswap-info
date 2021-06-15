c1a5e0137150785f32a9fe815fa2c9c9f83a435e
Mauro Henao <mauro.henaog@gmail.com>
2021-06-15 13:52:27 -0500

    redirectiong

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -4,9 +4,9 @@ import { Flex } from 'rebass'
 import Link from '../Link'
 
 const links = [
-  { url: 'https://pancakeswap.finance', text: 'About' },
-  { url: 'https://docs.pancakeswap.finance', text: 'Docs' },
-  { url: 'https://github.com/pancakeswap/pancakeswap-info', text: 'Code' },
+  { url: 'https://zoeswap.finance', text: 'About' },
+  { url: 'https://docs.zoeswap.finance', text: 'Docs' },
+  { url: 'https://github.com/zoe-cash/zoeswap-info', text: 'Code' },
 ]
 
 const FooterLink = ({ children, ...rest }) => (
