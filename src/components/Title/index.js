cbe9a10afd91e3c862bb89a798ea8c9a1b42de5f
Mauro Henao <mauro.henaog@gmail.com>
2021-06-15 13:59:30 -0500

    .

--- src/components/Title/index.js
+++ src/components/Title/index.js
@@ -5,7 +5,7 @@ import styled from 'styled-components'
 import { Flex } from 'rebass'
 import Link from '../Link'
 import { RowFixed } from '../Row'
-import Logo from '../../assets/logo-white.png'
+import Logo from '../../assets/logo.png'
 
 const TitleWrapper = styled.div`
   text-decoration: none;
@@ -32,7 +32,10 @@ export default function Title() {
       <Flex alignItems="center">
         <RowFixed>
           <UniIcon id="link" onClick={() => history.push('/')}>
-            <img width={'150px'} src={Logo} alt="logo" />
+            <img width={'24px'} src={Logo} alt="logo" style={{alignSelf:'center'}} />
+            <text style={{color:'white', fontSize:'1.3rem', fontStyle:'bold', fontWeight:'bolder', marginLeft:'0.3rem'}}>
+              ZOE SWAP
+            </text>            
           </UniIcon>
         </RowFixed>
       </Flex>
